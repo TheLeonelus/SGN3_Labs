6c7ff3eb141b2d11cc707f42e8defdb1ffa10c34
TheLeonelus <aloneTL@protonmail.ch>
2021-09-20 00:36:54 +0300

    modified:   Lab01.c 	modified:   Lab01_1.c
    Внесены следующие правки:
    - Оптимизирована память массива
    - Убраны излишние комментарии
    - Исправлена табуляция
    - Оптимизированы вычисления в части с прямой

--- Lab01.c
+++ Lab01.c
@@ -4,7 +4,7 @@ int xPointInput(int number)
 {
     int coord;  // Input of X-coord
     printf("\nInput point %i%s%i%s",number, ":\nx", number, ": ");
-    scanf("%i", &coord);
+    scanf("%d", &coord);
     return coord;
 }
 
@@ -12,38 +12,40 @@ int yPointInput(int number)
 {
     int coord;  // Input of Y-coord
     printf("\ny%i%s", number, ": ");
-    scanf("%i", &coord);
+    scanf("%d", &coord);
     return coord;
 }
 
-int rightPrint(void) // 2.3.2 output
+int rightPrint(void) // Вывод случая "Точка справа от вектора"
 {
     printf("\nPoint is to the right side from vector (p1,p2).");
 }
 
-int leftPrint(void) // 2.3.1 output
+int leftPrint(void) // Вывод случая "Точка слева от вектора"
 {
     printf("\nPoint is to the left side from vector (p1,p2).");
 }
 
-int includePrint(void) // 2.4 output
+int includePrint(void) // Точка пренадлежит линии
 {
     printf("\nLine includes the point.");
 }
 
-int inappropriateInputPrint(void) // 1.2/2.4
+int inappropriateInputPrint(void) // Точка лежит на прямой(-ых)
 {
     printf("Inappropriate input!");
 }
 
 int isIncludeCheck(int x1, int x2, int x3, int y1, int y2, int y3)
 {
-    if((x1==x2)&&(y1==y2)) return 1; // 1.2
-    if(((x1==x2)&&(x2==x3))||(((y1==y2)&&(y2==y3))||(y3==((x3-x2)*(y1-y2))/(x1-x2)+y2))) return 2; // 2.4.1
+    if((x1==x2)&&(y1==y2))
+        return 1; // Неверный ввод
+    if(((x1==x2)&&(x2==x3))||(((y1==y2)&&(y2==y3))||(y3==((x3-x2)*(y1-y2))/(x1-x2)+y2)))
+        return 2; // 3 точки лежат на одной прямой
     return 0;
 }
 
-int sectorPrint(int n) // 3.1 output
+int sectorPrint(int n) // Вывод номера сектора
 {
     printf("\nPoint is in the №%i%s", n, " sector.");
 }
@@ -61,67 +63,77 @@ int main(void)
     scanf("%d", &n);
     switch(n) {
         case 3: { // Line-case
-            int array[4][3]; // Array with point-coordinates
+            int array[3][2]; // Array with point-coordinates
             int k; // Cycle`s iteration | Number of point
             for (k=1; k<=3; ++k) { // fullfilling the array
-            array[k][1]=xPointInput(k);
-            array[k][2]=yPointInput(k);
-        }  
-            switch(isIncludeCheck(array[1][1], array[2][1], array[3][1], array[1][2], array[2][2], array[3][2])) {
-                case 0:
-                {
-                    if((array[1][1]==array[2][1])) {
-                        if(array[1][2]>array[2][2]) {
-                            if(array[1][1]<array[3][1]) return leftPrint();
+                array[k][0]=xPointInput(k);
+                array[k][1]=yPointInput(k);
+                printf("\nx: %d", array[k][0]);
+                printf("\ny: %d", array[k][1]);
+            }  
+            switch(isIncludeCheck(array[0][0], array[1][0], array[2][0], array[0][1], array[1][1], array[2][1])) {
+                case 0: {
+                    if((array[0][0]==array[1][0])) {
+                        if(array[0][1]>array[1][1]) {
+                            if(array[0][0]<array[2][0])
+                                return leftPrint();
                             return rightPrint();
                         }
-                        if(array[1][1]<array[3][1]) return rightPrint();
+                        if(array[0][0]<array[2][0])
+                            return rightPrint();
                         return leftPrint();
                     }
-                    if((array[2][1]-array[1][1])>0) {
-                        if(array[3][2]<(((array[3][1]-array[2][1])*(array[1][2]-array[2][2]))/(array[1][1]-array[2][1])+array[2][2])) return rightPrint();
+                    int y13 = ((array[2][0]-array[1][0])*(array[0][1]-array[1][1]))/(array[0][0]-array[1][0])+array[1][1];
+                    if((array[1][0]-array[0][0])>0) {
+                        if(array[2][1]<y13)
+                            return rightPrint();
                         return leftPrint();
                     }
-                    if(array[3][2]<(((array[3][1]-array[2][1])*(array[1][2]-array[2][2]))/(array[1][1]-array[2][1])+array[2][2])) return leftPrint();
+                    if(array[2][1]<y13)
+                        return leftPrint();
                     return rightPrint();
                     }
                 }
                 case 1: {
-                    return inappropriateInputPrint(); // 1.2
+                    return inappropriateInputPrint();
                 }
                 case 2: {
                     return includePrint();
                 }
             }
         case 4: { // triangle-case
-            int array[5][3]; // Array with point-coordinates
+            int array[4][2]; // Array with point-coordinates
             int k; // Cycle`s iteration | Number of point
             for (k=1; k<=4; ++k) { // fullfilling the array
-                array[k][1]=xPointInput(k);
-                array[k][2]=yPointInput(k);
+                array[k][0]=xPointInput(k);
+                array[k][1]=yPointInput(k);
             }
-            switch(isIncludeCheck(array[1][1], array[2][1], array[3][1], array[1][2], array[2][2], array[3][2])) {
+            switch(isIncludeCheck(array[0][0], array[1][0], array[2][0], array[0][1], array[1][1], array[2][1])) {
                 case 1: {
-                    return inappropriateInputPrint(); // 1.2
+                    return inappropriateInputPrint();
                 }
                 case 2: {
-                    return inappropriateInputPrint(); // 1.2
+                    return inappropriateInputPrint();
                 }
                 case 0: {
-                    if((isIncludeCheck(array[1][1], array[2][1], array[4][1], array[1][2], array[2][2], array[4][2])==0)&&(isIncludeCheck(array[2][1], array[3][1], array[4][1], array[2][2], array[3][2], array[4][2])==0)&&(isIncludeCheck(array[1][1], array[3][1], array[4][1], array[1][2], array[3][2], array[4][2])==0)) {
-                        if(isSectorCheck(array[1][1], array[3][1], array [4][1], array[1][2], array[3][2], array[4][2])==1) {
-                            if(isSectorCheck(array[1][1], array[2][1], array [4][1], array[1][2], array[2][2], array[4][2])==1) return sectorPrint(1); // 3.1.1
-                            if(isSectorCheck(array[2][1], array[3][1], array [4][1], array[2][2], array[3][2], array[4][2])==1) return sectorPrint(6); // 3.1.6
-                            return sectorPrint(5); // 3.1.5
+                    if((isIncludeCheck(array[0][0], array[1][0], array[3][0], array[0][1], array[1][1], array[3][1])==0)&&(isIncludeCheck(array[1][0], array[2][0], array[3][0], array[1][1], array[2][1], array[3][1])==0)&&(isIncludeCheck(array[0][0], array[2][0], array[3][0], array[0][1], array[2][1], array[3][1])==0)) {
+                        if(isSectorCheck(array[0][0], array[2][0], array [3][0], array[0][1], array[2][1], array[3][1])==1) {
+                            if(isSectorCheck(array[0][0], array[1][0], array [3][0], array[0][1], array[1][1], array[3][1])==1)
+                                return sectorPrint(1);
+                            if(isSectorCheck(array[1][0], array[2][0], array [3][0], array[1][1], array[2][1], array[3][1])==1)
+                                return sectorPrint(6);
+                            return sectorPrint(5);
                         }
-                        if(isSectorCheck(array[1][1], array[2][1], array [4][1], array[1][2], array[2][2], array[4][2])==1) {
-                            if(isSectorCheck(array[2][1], array[3][1], array [4][1], array[2][2], array[3][2], array[4][2])==1) return sectorPrint(2); // 3.1.2
-                            return sectorPrint(3); // 3.1.3
+                        if(isSectorCheck(array[0][0], array[1][0], array [3][0], array[0][1], array[1][1], array[3][1])==1) {
+                            if(isSectorCheck(array[1][0], array[2][0], array[3][0], array[1][1], array[2][1], array[3][1])==1)
+                                return sectorPrint(2);
+                            return sectorPrint(3);
                         }
-                        if(isSectorCheck(array[2][1], array[3][1], array [4][1], array[2][2], array[3][2], array[4][2])==1) return sectorPrint(7); // 3.1.7
-                        return sectorPrint(4); // 3.1.4
+                        if(isSectorCheck(array[1][0], array[2][0], array [3][0], array[1][1], array[2][1], array[3][1])==1)
+                            return sectorPrint(7);
+                        return sectorPrint(4);
                     }
-                    return includePrint(); // 2.4
+                    return includePrint();
                 }
             }          
         }
@@ -130,20 +142,6 @@ int main(void)
 
 
 
-// 1.1 - Неверное количество точек
-// 1.2 - Вершины совпадают
-// 2.3.1 - Точка слева от вектора (p1,p2)
-// 2.3.2 - Точка справа от вектора (p1,p2)
-// 2.4 - Точка принадлежит линии
-// 2.4.1 - 3 точки лежат на одной прямой
-// 3.1.1 - 1 сектор
-// 3.1.2 - 2 сектор
-// 3.1.3 - 3 сектор
-// 3.1.4 - 4 сектор
-// 3.1.5 - 5 сектор
-// 3.1.6 - 6 сектор
-// 3.1.7 - 7 сектор
-// ____________________________________
 // Сектора:
 // 1 - Угловой точки p1
 // 2 - Боковой прямой p1-p2
