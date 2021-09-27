f0573a8ee1f43a91eb25d2f7603177aa8255905d
TheLeonelus <aloneTL@protonmail.ch>
2021-09-27 12:01:33 +0300

    Исправлен ввод/Вывод

--- Lab02.c
+++ Lab02.c
@@ -2,42 +2,31 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define INAPPROPRIATEINPUT "\nInapprorpiate input."
+#define INAPPROPRIATEINPUT "\nInapprorpiate input.\n"
 
-double xValuesOutput(int n);
-
+double xValuesInput();
+double xValuesOutput();
 double yValuesOutput();
-
 double expressionFunction(double x);
-
 double yArrayZeroing();
+int zeroCheck();
 
-double array[2][3]; // array containing X
-
+double array[3][3]; // Возможно ли сделать динамическим?
 int k=1;
+int cycleType=1;
 
 double main(void)
 {
-    char s[3];
-    printf_s("\nInput value of x c [1;4]");
-    do
-    {
-        
-        printf_s("\nx%d%s", k, ": ");
-        scanf_s("%lf", &array[k-1][0], (unsigned)_countof(s));
-        // printf_s("\n%.3lf", array[k]);
-        if((array[k-1][0]>4.0)||(array[k-1][0]<1.0)) printf("%s", INAPPROPRIATEINPUT);
-        else ++k;
-    }
-    while (k<=3);
-    xValuesOutput(1);
+    xValuesInput();
+    zeroCheck();
+    xValuesOutput();
     for(k=0;k<=3;++k)
     {
         array[k][1] = expressionFunction(array[k][0]);
     }
     yValuesOutput();
     yArrayZeroing();
-    xValuesOutput(2);
+    xValuesOutput();
     while(k<=3)
     {
         array[k][1] = expressionFunction(array[k][0]);
@@ -45,7 +34,7 @@ double main(void)
     }
     yValuesOutput();
     yArrayZeroing();
-    xValuesOutput(3);
+    xValuesOutput();
     do
     {
         array[k][1] = expressionFunction(array[k][0]);
@@ -55,9 +44,38 @@ double main(void)
     yValuesOutput();
 }
 
-double xValuesOutput(int n)
+double xValuesInput()
 {
-    switch(n)
+    double a;
+    double b;
+    do
+    {
+    printf_s("\nInput left border of values: ");
+    scanf_s("%lf", &a);
+    printf_s("\nInput right border of values: ");
+    scanf_s("%lf", &b);
+    if (a>b)
+        printf_s("%s", INAPPROPRIATEINPUT);
+    else ++k;
+    }
+    while(k<2);
+    k=1;
+    printf_s("\nInput value of x c [%3.3lf%s%3.3lf%s", a, ";", b, "]");
+    do
+    {
+        
+        printf_s("\nx%d%s", k, ": ");
+        scanf_s("%lf", &array[k-1][0]);
+        if((array[k-1][0]>b)||(array[k-1][0]<a))
+            printf_s("%s", INAPPROPRIATEINPUT);
+        else ++k;
+    }
+    while (k<=3);
+}
+
+double xValuesOutput()
+{
+    switch(cycleType)
     {
         case 1:
         {
@@ -75,12 +93,20 @@ double xValuesOutput(int n)
             break;
         }
     }
-    printf_s("\n x | %3.3lf%s%3.3lf%s%3.3lf%s", array[0][0], " | ", array[1][0], " | ", array[2][0], " |");
+    printf_s("\n   x  | %3.3lf%s%3.3lf%s%3.3lf%s", array[0][0], " | ", array[1][0], " | ", array[2][0], " |");
 }
 
 double yValuesOutput()
 {
-    printf_s("\n y | %3.3lf%s%3.3lf%s%3.3lf%s", array[0][1], " | ", array[1][1], " | ", array[2][1], " |\n");
+    printf_s("\n f(x) | ");
+    for(int i=0;i<=2;i++)
+    {
+        if(array[i][2]==1)
+            printf_s("NaN | ");
+        else
+            printf_s("%3.3lf%s", array[i][1], " | ");
+    }
+    printf_s("\n");
 }
 
 double expressionFunction(double x)
@@ -96,9 +122,23 @@ double yArrayZeroing()
         array[k][1] = 0;
     }
     k=0;
+    cycleType++;
 }
 
-
+int zeroCheck()
+{
+    int zero = 0;
+    for(int i=0;i<=2;i++)
+    {
+        if(abs(array[i][0])<0.000001)
+        {
+             array[i][2]=1;
+        }
+            printf_s("\nCheck: %lf", array[i][0]);
+            printf_s("\nCheck: %lf", array[i][2]);
+        }
+    
+}
 
 // Цикл из 3 итераций
 // Ввод x1-x4
