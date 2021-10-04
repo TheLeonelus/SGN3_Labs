3426d8b0246384ba6e38c55771513fabed5bcf26
TheLeonelus <aloneTL@protonmail.ch>
2021-10-04 22:53:09 +0300

    Удалены лишние переменные, добавлены комментарии

--- Lab03.c
+++ Lab03.c
@@ -9,27 +9,27 @@ double xCalculation(int i);
 double fxCalculation(double x);
 double integralCalculation();
 
-double a, b, H, E, pastAreaValue, currentAreaValue, difference;
+double a, b, H, E;
 int splitAmount, k;
 
 int main()
 {
     integralBordersInput();
     precisionInput();
-    splitAmount = 2;
+    splitAmount = 2; // Количество участков разбиения
     stepSize();
     k=0;
+    double pastAreaValue, currentAreaValue;
     pastAreaValue = currentAreaValue = integralCalculation();
     printf_s("");
     while(k<1)
     {
         pastAreaValue = currentAreaValue;
-        splitAmount = 2*splitAmount;
+        splitAmount = 2*splitAmount; // Увеличение точности подсчётом
         stepSize();
         currentAreaValue = integralCalculation();
-        difference = pastAreaValue-currentAreaValue;
         printf_s("");
-        if(E>difference)
+        if(E>(pastAreaValue-currentAreaValue))
         {
             k++;
         }
@@ -61,12 +61,12 @@ void precisionInput()
 
 void stepSize()
 {
-    H = (b-a)/(2*splitAmount);
+    H = (b-a)/(2*splitAmount); // Приращение функции
 }
 
 double xCalculation(int n)
 {
-    double result = a+n*H;
+    double result = a+n*H; 
     return result;
 }
 
@@ -80,19 +80,19 @@ double integralCalculation()
 {
     double array[2][(2*splitAmount)+1], firstSum, secondSum;
     firstSum = secondSum = 0;
-    for(int i=0;i<=((2*splitAmount)+1);i++)
+    for(int i=0;i<=((2*splitAmount)+1);i++) // Заполнение xi и f(xi)
     {
         array[0][i] = xCalculation(i);
         array[1][i] = fxCalculation(array[0][i]);
     }
-    for(int i=1;i<=(splitAmount);i++)
+    for(int i=1;i<=(splitAmount);i++) // Вычисление суммы нечётных f(x)
     {
         firstSum = firstSum + array[1][2*i-1];
     }
-    for(int i=1;i<=(splitAmount-1);i++)
+    for(int i=1;i<=(splitAmount-1);i++) // Вычисление суммы чётных f(x)
     {
         secondSum = secondSum + array[1][2*i];
     }
-    double result = (H/3)*(array[1][0]+4*firstSum+2*secondSum+array[1][2*splitAmount+1]);
+    double result = (H/3)*(array[1][0]+4*firstSum+2*secondSum+array[1][2*splitAmount+1]); // Формула Симпсона
     return result;
 }
