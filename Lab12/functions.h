6da82baccceccd9f55f693c6e7e87643fb226e02
TheLeonelus <aloneTL@protonmail.ch>
2021-12-26 22:16:51 +0300

    Вынесены структура и утилитарные функции в отдельные .h

--- Lab12/functions.h
+++ Lab12/functions.h
@@ -1,4 +1,5 @@
 #ifndef _ADDITONAL_FUNCTIONS
+#define _ADDITIONAL_FUNCTIONS
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,34 +7,16 @@
 #include <synchapi.h>
 #include <string.h>
 #include <conio.h>
+#include <io.h>
 
-#define MAX_SIZE 30
+#include "struct.h"
 
-typedef struct game
-{
-    char name[MAX_SIZE];
-    char developer[MAX_SIZE];
-    char publisher[MAX_SIZE];
-    int rating;
-}game;
-
-#define _ADDITIONAL_FUNCTIONS
-void gotoxy(int x, int y);
 int menuInOut();
 game structInput();
 int structWriteInFile(FILE* f);
 int filePrint(FILE* f);
 int fieldToSortChoose();
-int getFileSizeInBytes(FILE* f);
-int getElementsCount(FILE* f);
-void setFileSize(FILE* f, long newSize);
-game readElementFromFile(FILE* f, int index);
-void writeElementInFile(FILE* f, const game student, int index);
 int removeElementFromFile(FILE* f);
-int compareByName(game record1, game record2);
-int compareByDeveloper(game record1, game record2);
-int compareByPublisher(game record1, game record2);
-int compareByRating(game record1, game record2);
 void sortFile(FILE* f, int (*cmp)(game record1, game record2));
-void swapRecordsInFile(FILE* f, int index1, int index2);
+
 #endif
