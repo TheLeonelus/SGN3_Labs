3286f80a903c551e20347b80f38225a4a310c992
TheLeonelus <aloneTL@protonmail.ch>
2021-12-26 16:39:34 +0300

    Лабораторная №12

---
+++ Lab12/functions.h
@@ -0,0 +1,39 @@
+#ifndef _ADDITONAL_FUNCTIONS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <synchapi.h>
+#include <string.h>
+#include <conio.h>
+
+#define MAX_SIZE 30
+
+typedef struct game
+{
+    char name[MAX_SIZE];
+    char developer[MAX_SIZE];
+    char publisher[MAX_SIZE];
+    int rating;
+}game;
+
+#define _ADDITIONAL_FUNCTIONS
+void gotoxy(int x, int y);
+int menuInOut();
+game structInput();
+int structWriteInFile(FILE* f);
+int filePrint(FILE* f);
+int fieldToSortChoose();
+int getFileSizeInBytes(FILE* f);
+int getElementsCount(FILE* f);
+void setFileSize(FILE* f, long newSize);
+game readElementFromFile(FILE* f, int index);
+void writeElementInFile(FILE* f, const game student, int index);
+int removeElementFromFile(FILE* f);
+int compareByName(game record1, game record2);
+int compareByDeveloper(game record1, game record2);
+int compareByPublisher(game record1, game record2);
+int compareByRating(game record1, game record2);
+void sortFile(FILE* f, int (*cmp)(game record1, game record2));
+void swapRecordsInFile(FILE* f, int index1, int index2);
+#endif
