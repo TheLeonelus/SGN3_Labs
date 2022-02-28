25d622a87411be52da7909921afb22b834a161cf
TheLeonelus <aloneTL@protonmail.ch>
2022-02-28 16:06:22 +0300

    Измеенён цвет окна

--- Term2/Lab2_01/main.cpp
+++ Term2/Lab2_01/main.cpp
@@ -1,4 +1,4 @@
-#include "Calculator.h"
+#include "calculator.h"
 
 int main(int argc, char *argv[])
 {
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
     Calculator calculator;
     calculator.setWindowTitle("Calculator");
     calculator.resize(230, 200);
+    calculator.setStyleSheet("background: black;");
     calculator.show();
     return a.exec();
 }
